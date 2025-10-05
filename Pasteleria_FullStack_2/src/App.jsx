f48e22ea921da71fe2a7f6251f3acdeb31e1376e
BrazenGhost630 <fel.vargaso@duocuc.cl>
2025-10-05 18:16:35 -0300

    Iniciando Login

--- Pasteleria_FullStack_2/src/App.jsx
+++ Pasteleria_FullStack_2/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { Route,Routes } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Formulario from './Login/Formulario'
 
 function App() {
   
 
   return (
      <>
+     
       <Routes>
+        
         <Route path='/' element={<h1>Hola Mundo</h1>}/>
         <Route path='/about' element={<h1>About</h1>}/>
         <Route path='/contact' element={<h1>Contact</h1>}/>
+         <Route path='/login' element={<Formulario />} />
       </Routes>
       </>
   )
--- Pasteleria_FullStack_2/src/main.jsx
+++ Pasteleria_FullStack_2/src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 
 import './index.css'
 import App from './App.jsx'
+import { LoginProvider } from './apis/Api_Felix.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+    <LoginProvider>
+      <App />
+    </LoginProvider>
     </BrowserRouter>
     
   </StrictMode>,
